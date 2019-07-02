608968024deb84b6e413a5ea9cb7470186e0a34a
jasminegmp <jasminekgump@gmail.com>
2019-07-02 16:51:59 -0700

    Environment variable settings for Firebase

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import app from 'firebase/app';
+
+const config = require('./config').default;
+
+var firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.API_ID,
+  };
 
 
-const Firebase = () => {
-    return (
-        <div>Firebase</div>
-    );
-};
+// Initialize Firebase
+Firebase.initializeApp(firebaseConfig);
 
 export default Firebase;
